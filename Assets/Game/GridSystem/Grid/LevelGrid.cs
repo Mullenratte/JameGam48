1d14f2de9aee89d0883191bff038c4fe1553d24f
Mullenratte <Max.Blanc@web.de>
2025-04-17 15:12:48 +0200

    LevelGrid now spawns 3D Objects instead of debug sprites (+ camera changes)

--- Assets/Game/GridSystem/Grid/LevelGrid.cs
+++ Assets/Game/GridSystem/Grid/LevelGrid.cs
@@ -21,7 +21,7 @@ public class LevelGrid : MonoBehaviour {
     private Tile[,] tileGrid;
     private Visualization visualization;
 
-    [SerializeField] Transform debug_tilePrefab1, debug_tilePrefab2;
+    [SerializeField] Transform gridMeshObjectPrefab;
     [SerializeField] private Transform gridDebugObjectPrefab;
     [SerializeField] private Transform flyPrefab;
 
@@ -51,20 +51,18 @@ public class LevelGrid : MonoBehaviour {
 
         GenerateStartObjects(0);
 
-        for (int i = 0; i < TestAppends; i++)
-        {
+        for (int i = 0; i < TestAppends; i++) {
             TryAppendAsync();
         }
     }
 
-    private void GenerateStartObjects(int startZ)
-    {
-        for (int z = startZ; z < tileGrid.GetLength(1); z++)
-        {
-            for (int x = 0; x < tileGrid.GetLength(0); x++)
-            {
+    private void GenerateStartObjects(int startZ) {
+        for (int z = startZ; z < tileGrid.GetLength(1); z++) {
+            for (int x = 0; x < tileGrid.GetLength(0); x++) {
                 GridPosition gridPosition = new GridPosition(x, z);
                 GridObject gridObject = new GridObject(GridSystem, gridPosition, tileGrid[x, z]);
+
+
 
                 //Transform model = Instantiate(debug_tilePrefab1, GridSystem.GetWorldPosition(gridPosition), Quaternion.identity);
                 //if (model != null) {
@@ -73,22 +71,27 @@ public class LevelGrid : MonoBehaviour {
                 //}
 
 
-                Transform debugObjTransform = Instantiate(gridDebugObjectPrefab, GridSystem.GetWorldPosition(gridPosition), Quaternion.identity);
-                DEBUG_GridObject debugObj = debugObjTransform.GetComponent<DEBUG_GridObject>();
-                debugObj.SetGridObject(gridObject);
-                if (debugObjTransform != null)
-                {
-                    debugObjTransform.SetParent(this.transform);
-                    debugObjTransform.gameObject.layer = LayerMask.NameToLayer("Grid");
+                Transform objTransform = Instantiate(gridMeshObjectPrefab, GridSystem.GetWorldPosition(gridPosition), Quaternion.identity);
+                MeshGridObject meshObj= objTransform.GetComponent<MeshGridObject>();
+                meshObj.SetGridObject(gridObject);
+                if (objTransform != null) {
+                    objTransform.SetParent(this.transform);
+                    objTransform.gameObject.layer = LayerMask.NameToLayer("Grid");
                 }
 
-                if (tileGrid[x, z].blockType == Tile.BlockType.FlySpawn)
-                {
+                //Transform debugObjTransform = Instantiate(gridDebugObjectPrefab, GridSystem.GetWorldPosition(gridPosition), Quaternion.identity);
+                //DEBUG_GridObject debugObj = debugObjTransform.GetComponent<DEBUG_GridObject>();
+                //debugObj.SetGridObject(gridObject);
+                //if (debugObjTransform != null) {
+                //    debugObjTransform.SetParent(this.transform);
+                //    debugObjTransform.gameObject.layer = LayerMask.NameToLayer("Grid");
+                //}
+
+                if (tileGrid[x, z].blockType == Tile.BlockType.FlySpawn) {
                     Transform flyObjTransform = Instantiate(flyPrefab, GridSystem.GetWorldPosition(gridPosition), Quaternion.identity);
                 }
 
-                if (tileGrid[x, z].blockType == Tile.BlockType.ItemSpawn)
-                {
+                if (tileGrid[x, z].blockType == Tile.BlockType.ItemSpawn) {
                     int rnd = UnityEngine.Random.Range(0, items.Count);
                     Transform itemTransform = Instantiate(items[rnd].gameObject.transform, GridSystem.GetWorldPosition(gridPosition), Quaternion.identity);
                     itemTransform.position += Vector3.up;
@@ -97,12 +100,9 @@ public class LevelGrid : MonoBehaviour {
         }
     }
 
-    private IEnumerator GenerateObjects(int startZ)
-    {
-        for (int z = startZ; z < tileGrid.GetLength(1); z++)
-        {
-            for (int x = 0; x < tileGrid.GetLength(0); x++)
-            {
+    private IEnumerator GenerateObjects(int startZ) {
+        for (int z = startZ; z < tileGrid.GetLength(1); z++) {
+            for (int x = 0; x < tileGrid.GetLength(0); x++) {
                 GridPosition gridPosition = new GridPosition(x, z);
                 GridObject gridObject = new GridObject(GridSystem, gridPosition, tileGrid[x, z]);
 
@@ -121,13 +121,11 @@ public class LevelGrid : MonoBehaviour {
                     debugObjTransform.gameObject.layer = LayerMask.NameToLayer("Grid");
                 }
 
-                if (tileGrid[x, z].blockType == Tile.BlockType.FlySpawn)
-                {
+                if (tileGrid[x, z].blockType == Tile.BlockType.FlySpawn) {
                     Transform flyObjTransform = Instantiate(flyPrefab, GridSystem.GetWorldPosition(gridPosition), Quaternion.identity);
                 }
 
-                if (tileGrid[x, z].blockType == Tile.BlockType.ItemSpawn)
-                {
+                if (tileGrid[x, z].blockType == Tile.BlockType.ItemSpawn) {
                     int rnd = UnityEngine.Random.Range(0, items.Count);
                     Transform itemTransform = Instantiate(items[rnd].gameObject.transform, GridSystem.GetWorldPosition(gridPosition), Quaternion.identity);
                     itemTransform.position += Vector3.up;
@@ -145,23 +143,19 @@ public class LevelGrid : MonoBehaviour {
 
     }
 
-    public void TryAppendAsync()
-    {   
-        if (!isAppending)
-        {
+    public void TryAppendAsync() {
+        if (!isAppending) {
             StartCoroutine(AppendNewSectionAsync());
         }
     }
 
-    private IEnumerator AppendNewSectionAsync()
-    {
+    private IEnumerator AppendNewSectionAsync() {
         isAppending = true;
 
         Tile[,] newSection = null;
 
         Debug.Log("Appending new section...");
-        yield return RunInBackground(() =>
-        {
+        yield return RunInBackground(() => {
             newSection = Generator.GeneratePaths(connectXs, tileGrid.GetLength(1));
             connectXs = Generator.GetConnectXs();
         });
@@ -171,25 +165,22 @@ public class LevelGrid : MonoBehaviour {
         tileGrid = CombineTileGrids(tileGrid, newSection);
         GridSystem.UpdateDimensions(tileGrid.GetLength(0), tileGrid.GetLength(1));
 
-        for (int x = 0; x < tileGrid.GetLength(0); x++)
-        {
+        for (int x = 0; x < tileGrid.GetLength(0); x++) {
             if (!tileGrid[x, oldDepth - 1].isConnection) continue;
             Tile southTile = tileGrid[x, oldDepth - 1];
             Tile northTile = tileGrid[x, oldDepth];
             southTile.SetNeighbor(Direction.North, northTile);
             northTile.SetNeighbor(Direction.South, southTile);
         }
-        
+
         StartCoroutine(GenerateObjects(oldDepth));
 
         isAppending = false;
     }
 
-    private IEnumerator RunInBackground(Action action)
-    {
+    private IEnumerator RunInBackground(Action action) {
         bool done = false;
-        System.Threading.Thread thread = new System.Threading.Thread(() =>
-        {
+        System.Threading.Thread thread = new System.Threading.Thread(() => {
             action();
             done = true;
         });
@@ -199,13 +190,10 @@ public class LevelGrid : MonoBehaviour {
             yield return null;
     }
 
-    public void RemoveFirstRow()
-    {
+    public void RemoveFirstRow() {
         Tile[,] newGrid = new Tile[tileGrid.GetLength(0), tileGrid.GetLength(1) - 1];
-        for (int x = 0; x < tileGrid.GetLength(0); x++)
-        {
-            for (int z = 1; z < tileGrid.GetLength(1); z++)
-            {
+        for (int x = 0; x < tileGrid.GetLength(0); x++) {
+            for (int z = 1; z < tileGrid.GetLength(1); z++) {
                 newGrid[x, z - 1] = tileGrid[x, z];
             }
         }
@@ -213,33 +201,27 @@ public class LevelGrid : MonoBehaviour {
         zOffset++;
     }
 
-    Tile[,] CombineTileGrids(Tile[,] original, Tile[,] toAppend)
-    {
+    Tile[,] CombineTileGrids(Tile[,] original, Tile[,] toAppend) {
         int originalRows = original.GetLength(0);
         int originalCols = original.GetLength(1);
         int appendRows = toAppend.GetLength(0);
         int appendCols = toAppend.GetLength(1);
 
-        if (originalRows != appendRows)
-        {
+        if (originalRows != appendRows) {
             Debug.LogError("Tile grids couldn't be combined!");
             return null;
         }
 
         Tile[,] combined = new Tile[originalRows, originalCols + appendCols];
 
-        for (int row = 0; row < originalRows; row++)
-        {
-            for (int col = 0; col < originalCols; col++)
-            {
+        for (int row = 0; row < originalRows; row++) {
+            for (int col = 0; col < originalCols; col++) {
                 combined[row, col] = original[row, col];
             }
         }
 
-        for (int row = 0; row < appendRows; row++)
-        {
-            for (int col = 0; col < appendCols; col++)
-            {
+        for (int row = 0; row < appendRows; row++) {
+            for (int col = 0; col < appendCols; col++) {
                 combined[row, originalCols + col] = toAppend[row, col];
             }
         }
@@ -252,18 +234,15 @@ public class LevelGrid : MonoBehaviour {
         return tileGrid;
     }
 
-    public List<int> GetConnectXs()
-    {
+    public List<int> GetConnectXs() {
         return connectXs;
     }
 
-    public int GetWidth()
-    {
+    public int GetWidth() {
         return tileGrid.GetLength(0);
     }
 
-    public int GetDepth()
-    {
+    public int GetDepth() {
         return tileGrid.GetLength(1);
     }
 }
--- Assets/Game/GridSystem/Grid/MeshGridObject.cs
+++ Assets/Game/GridSystem/Grid/MeshGridObject.cs
@@ -1,19 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
-public class DEBUG_GridObject : MonoBehaviour {
-    [SerializeField] private Color n1_c, n2_c, n3_c, n4_c;
-    
-    private GridObject gridObject;    
-    public Sprite[] sprites;
-    private Color activeColor;
-    private bool hasFallen = false;
+public class MeshGridObject : MonoBehaviour {    
+    private GridObject gridObject;
+    [SerializeField] GameObject[] tileMeshPrefabs;
 
-    GameObject spriteDisplayObject;
-    MeshRenderer _meshRenderer;
-    SpriteRenderer _spriteRenderer;
+    GameObject activeMeshPrefab;
+    private bool hasFallen = false;
 
     public void SetGridObject(GridObject gridObject) {
         this.gridObject = gridObject;
@@ -26,133 +20,125 @@ public class DEBUG_GridObject : MonoBehaviour {
             }
         }
 
-        switch (neighbors) {
-            case 1: activeColor = n1_c; break;
-            case 2: activeColor = n2_c; break;
-            case 3: activeColor = n3_c; break;
-            case 4: activeColor = n4_c; break;
-        }
-        _meshRenderer.material.color = activeColor;
-
-        SetTopSprite();
+        SetMeshPrefab();
     }
 
-    private void SetTopSprite()
+    private void SetMeshPrefab()
     {
         int tileTypeIndex = gridObject.tile.GetTileType();
+
+        GameObject prefabToSpawn = null;
+        float yRotation = 0;
 
         switch (tileTypeIndex)
         {
             // Free tile
             case 0:
-                _spriteRenderer.sprite = sprites[0];
-                spriteDisplayObject.SetZRotation(0f);
+                prefabToSpawn = tileMeshPrefabs[0];
+                yRotation = 0f;
                 break;
 
             // Deadend tiles
             case 1:
-                _spriteRenderer.sprite = sprites[1];
-                spriteDisplayObject.SetZRotation(0f);
+                prefabToSpawn = tileMeshPrefabs[1];
+                yRotation = 180f;
                 break;
             case 2:
-                _spriteRenderer.sprite = sprites[1];
-                spriteDisplayObject.SetZRotation(90f);
+                prefabToSpawn = tileMeshPrefabs[1];
+                yRotation = 270f;
                 break;
             case 3:
-                _spriteRenderer.sprite = sprites[1];
-                spriteDisplayObject.SetZRotation(180f);
+                prefabToSpawn = tileMeshPrefabs[1];
+                yRotation = 0f;
                 break;
             case 4:
-                _spriteRenderer.sprite = sprites[1];
-                spriteDisplayObject.SetZRotation(270f);
+                prefabToSpawn = tileMeshPrefabs[1];
+                yRotation = 90f;
                 break;
 
             // Corner tiles
             case 5:
-                _spriteRenderer.sprite = sprites[2];
-                spriteDisplayObject.SetZRotation(0f);
+                prefabToSpawn = tileMeshPrefabs[2];
+                yRotation = 270f;
                 break;
             case 6:
-                _spriteRenderer.sprite = sprites[2];
-                spriteDisplayObject.SetZRotation(90f);
+                prefabToSpawn = tileMeshPrefabs[2];
+                yRotation = 0f;
                 break;
             case 7:
-                _spriteRenderer.sprite = sprites[2];
-                spriteDisplayObject.SetZRotation(180f);
+                prefabToSpawn = tileMeshPrefabs[2];
+                yRotation = 90f;
                 break;
             case 8:
-                _spriteRenderer.sprite = sprites[2];
-                spriteDisplayObject.SetZRotation(270f);
+                prefabToSpawn = tileMeshPrefabs[2];
+                yRotation = 180f;
                 break;
 
             // Straight tiles
             case 9:
-                _spriteRenderer.sprite = sprites[3];
-                spriteDisplayObject.SetZRotation(0f);
+                prefabToSpawn = tileMeshPrefabs[3];
+                yRotation = 0f;
                 break;
             case 10:
-                _spriteRenderer.sprite = sprites[3];
-                spriteDisplayObject.SetZRotation(90f);
+                prefabToSpawn = tileMeshPrefabs[3];
+                yRotation = 90f;
                 break;
 
             // T-junction tiles
             case 11:
-                _spriteRenderer.sprite = sprites[4];
-                spriteDisplayObject.SetZRotation(0f);
+                prefabToSpawn = tileMeshPrefabs[4];
+                yRotation = 0f;
                 break;
             case 12:
-                _spriteRenderer.sprite = sprites[4];
-                spriteDisplayObject.SetZRotation(90f);
+                prefabToSpawn = tileMeshPrefabs[4];
+                yRotation = 90f;
                 break;
             case 13:
-                _spriteRenderer.sprite = sprites[4];
-                spriteDisplayObject.SetZRotation(180f);
+                prefabToSpawn = tileMeshPrefabs[4];
+                yRotation = 180f;
                 break;
             case 14:
-                _spriteRenderer.sprite = sprites[4];
-                spriteDisplayObject.SetZRotation(270f);
+                prefabToSpawn = tileMeshPrefabs[4];
+                yRotation = 270f;
                 break;
 
             // Intersection tile
             case 15:
-                _spriteRenderer.sprite = sprites[5];
-                spriteDisplayObject.SetZRotation(0f);
+                prefabToSpawn = tileMeshPrefabs[5];
+                yRotation = 0f;
                 break;
 
             // Bridge tiles
             case 16:
-                _spriteRenderer.sprite = sprites[6];
-                spriteDisplayObject.SetZRotation(90f);
+                prefabToSpawn = tileMeshPrefabs[6];
+                yRotation = 90f;
                 break;
             case 17:
-                _spriteRenderer.sprite = sprites[6];
-                spriteDisplayObject.SetZRotation(0f);
+                prefabToSpawn = tileMeshPrefabs[6];
+                yRotation = 0f;
                 break;
             
             // Blocked tiles
             case 18:
-                _spriteRenderer.sprite = sprites[7];
-                spriteDisplayObject.SetZRotation(0f);
+                prefabToSpawn = tileMeshPrefabs[0];
+                yRotation = 0f;
                 break;
             case 19:
-                _spriteRenderer.sprite = sprites[8];
-                spriteDisplayObject.SetZRotation(0f);
+                prefabToSpawn = tileMeshPrefabs[0];
+                yRotation = 0f;
                 break;
             case 20:
-                _spriteRenderer.sprite = sprites[0];
-                spriteDisplayObject.SetZRotation(0f);
+                prefabToSpawn = tileMeshPrefabs[0];
+                yRotation = 0f;
                 break;
 
             default:
                 Debug.LogError("Invalid tile type index: " + tileTypeIndex);
                 break;
         }
-    }
 
-    private void Awake() {
-        _meshRenderer = GetComponent<MeshRenderer>();
-        this.spriteDisplayObject = transform.Find("SpriteDisplay").gameObject;
-        _spriteRenderer = this.spriteDisplayObject.GetComponent<SpriteRenderer>();
+        this.activeMeshPrefab = Instantiate(prefabToSpawn, this.transform);
+        activeMeshPrefab.SetYRotation(yRotation);
     }
 
     private void Update()
@@ -206,7 +192,7 @@ public class DEBUG_GridObject : MonoBehaviour {
 
         while (time < shakeDuration)
         {
-            float offsetX = Mathf.Sin(time * 40f) * shakeStrength;
+            float offsetX = Mathf.Sin(time * 40f * shakeStrength);
             obj.transform.position = originalPos + new Vector3(offsetX, 0f, 0f);
             time += Time.deltaTime;
             yield return null;
@@ -258,31 +244,5 @@ public class DEBUG_GridObject : MonoBehaviour {
         }
 
         obj.transform.position = targetPos;
-    }
-}
-
-public static class TransformExtensions
-{
-    /// <summary>
-    /// Setzt nur die Z-Rotation eines GameObjects, ohne X- und Y-Rotation zu verändern.
-    /// </summary>
-    /// <param name="transform">Das Transform-Objekt des GameObjects</param>
-    /// <param name="zAngle">Die neue Rotation in Grad um die Z-Achse</param>
-    public static void SetZRotation(this GameObject toTransform, float zAngle)
-    {
-        Vector3 currentEuler = toTransform.transform.rotation.eulerAngles;
-        currentEuler.z = zAngle;
-        toTransform.transform.rotation = Quaternion.Euler(currentEuler);
-    }
-
-    /// <summary>
-    /// Setzt nur die Y-Rotation eines GameObjects, ohne X- und Z-Rotation zu verändern.
-    /// </summary>
-    /// <param name="transform">Das Transform-Objekt des GameObjects</param>
-    /// <param name="yAngle">Die neue Rotation in Grad um die Y-Achse</param>
-    public static void SetYRotation(this GameObject toTransform, float yAngle) {
-        Vector3 currentEuler = toTransform.transform.rotation.eulerAngles;
-        currentEuler.y = yAngle;
-        toTransform.transform.rotation = Quaternion.Euler(currentEuler);
     }
 }
