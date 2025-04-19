b526affa5597464dc2ace2658329f96590fa2ac1
Mullenratte <Max.Blanc@web.de>
2025-04-19 08:50:57 +0200

    Camera zoom out depends on player speed

---
+++ Assets/Game/Camera/CameraController.cs
@@ -0,0 +1,29 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public static CameraController Instance;
+    CinemachineCamera cam;
+    [SerializeField] float originalCameraDistance;
+    CinemachinePositionComposer positionComposer;
+
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        } else {
+            Destroy(gameObject);
+        }
+        cam = GetComponent<CinemachineCamera>();
+        positionComposer = GetComponent<CinemachinePositionComposer>();
+    }
+
+    private void Start() {
+        transform.position = new Vector3(PlayerMovement.Instance.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+        positionComposer.CameraDistance = originalCameraDistance;
+    }
+
+    private void Update() {
+        positionComposer.CameraDistance = originalCameraDistance + PlayerMovement.Instance.MoveSpeed;
+    }
+}
